d403a72b65c4861e3640c6dd93e4658867a8c482
user <user@mbp.(none)>
2013-04-22 20:32:01 +0300

    init

---
+++ sites/all/libraries/ckeditor/config.js
@@ -0,0 +1,21 @@
+﻿/*
+Copyright (c) 2003-2013, CKSource - Frederico Knabben. All rights reserved.
+For licensing, see LICENSE.html or http://ckeditor.com/license
+*/
+
+CKEDITOR.editorConfig = function( config )
+{
+	// Define changes to default configuration here. For example:
+	// config.language = 'fr';
+	 config.uiColor = '#AADC6E';
+};
+CKEDITOR.stylesSet.add( 'my_styles',
+[
+    // Block-level styles
+    { name : 'Blue Title', element : 'h2', styles : { 'color' : 'Blue' } },
+    { name : 'Red Title' , element : 'h3', styles : { 'color' : 'Red' } },
+    { name : 'Rzzzed Title' , element : 'h3', styles : { 'color' : 'Red' } },
+    // Inline styles
+    { name : 'CSS Style', element : 'span', attributes : { 'class' : 'my_style' } },
+    { name : 'Marker: Yellow', element : 'span', styles : { 'background-color' : 'Yellow' } }
+]);
